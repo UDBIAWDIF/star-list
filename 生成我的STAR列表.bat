de292b5a4b1a5d2a7e883d2e4e7494967a9933e9
UID <UID@YEAH.NET>
2018-05-21 00:33:02 +0800

    Just update.

--- 生成我的STAR列表.bat
+++ 生成我的STAR列表.bat
@@ -1,6 +1,6 @@
 starred --username UDBIAWDIF --sort > star-list\README.md
 cd star-list
-git stage README.md 生成我的STAR列表.bat
+git stage "README.md" "生成我的STAR列表.bat"
 git commit -m "Just update."
 git push --progress "origin" master:master
 pause
